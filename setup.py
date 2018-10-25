d20e53428fd15842f75d072f8329b4143bd3c4a9
Obaro Odiete <edeas123@users.noreply.github.com>
2018-10-24 23:17:43 -0400

    fillin install_requires. Closes: #52 (#53)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='blogextractor',
-    version='0.0.1',
+    version='0.0.2',
     description='Crawl local blogs',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -13,7 +13,15 @@ setuptools.setup(
     author='Obaro Odiete',
     packages=setuptools.find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        'flask'
+        'bs4==0.0.1',
+        'Flask==1.0.2',
+        'Flask-RESTful==0.3.6',
+        'lxml==4.2.4',
+        'marshmallow==3.0.0b13',
+        'PyYAML==3.13',
+        'requests==2.19.1',
+        'urllib3==1.23',
+        'uwsgi==2.0.17.1'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
