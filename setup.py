d723c05d74e84f22c7736941542ff4db3742e91e
edeas123 <edeas123@gmail.com>
2018-11-29 03:22:23 -0500

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='blogextractor',
-    version='0.0.2',
+    version='0.0.3',
     description='Extract data from blogs',
     long_description=long_description,
     long_description_content_type="text/markdown",
