6536856bdb731ec9b94d3c26e90212fb7bb33ee9
edeas123 <edeas123@gmail.com>
2019-01-06 19:35:19 -0500

    use docker management commands instead

--- run_test.sh
+++ run_test.sh
@@ -2,10 +2,10 @@
 set -exo pipefail
 
 # build docker image
-docker build -t blogextractor:latest -f TestDockerfile .
+docker image build -t blogextractor:latest -f TestDockerfile .
 
 # run docker image
-docker run --name blogextractor_test -p 5000:5000 blogextractor:latest
+docker container run --name blogextractor_test -p 5000:5000 blogextractor:latest
 
 # stop and remove docker container
 docker container rm -f blogextractor_test
